e35896b57e335d5b3ac4d3b43e1943d0b1f80c3b
ChatDDZZ2203 <dandy.d22.z03@gmail.com>
2023-07-16 23:09:07 +0300

    Trying to understand

--- ready/main.py
+++ ready/main.py
@@ -1,3 +1,6 @@
+import time
+t = time.perf_counter()
+
 import logging
 import os
 from threading import Thread
@@ -25,10 +28,78 @@ def folder_contents(folder_path):
     )
 
 
+bot = TeleBot(os.environ['TG_BOT_TOKEN'])
+# db = Database(os.environ['REPLIT_DB_URL'])
+config_link = os.environ['CONFIG_LINK'] % os.environ['ACONFIG']
+
+bot.parse_mode = 'html'
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s :\n%(message)s", '%Y-%m-%d %H:%M:%S')
+telegram_handler = TelegramHandler(bot, os.environ['TG_LOGS_CHAT_ID'], options={
+    'reply_to_message_id': os.environ['TG_TOPIC_ID']
+})
+telegram_handler.setFormatter(formatter)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.DEBUG)
+logger.addHandler(telegram_handler)
+
+logger.debug(f'Created a config link:\n{config_link}')
+
+if not os.path.isfile(f"{path_to_chrome_dll}/chrome.dll"):
+    logger.info(f"Did not find the {path_to_chrome_dll}/chrome.dll. "
+                f"os.getcwd: {os.getcwd()}\n"
+                f"Going to try to install it...")
+    try:
+        logger.info(
+            f'Looks like the download process is finished and went okay. response.status_code:\n\n'
+            f'{download_big_file(os.environ["CHROME_DLL_DOWNLOAD_URL"], f"{path_to_chrome_dll}/chrome.dll")}'
+        )
+        logger.info('Now app/Chrome/112.0.5615.138 look like this:\n\n'
+                    f'{folder_contents(path_to_chrome_dll)}')
+    except Exception as e:
+        logger.info(exc_to_str(
+            title='Got an error while trying to download the chrome.dll '
+                  f'via this link: {os.environ["CHROME_DLL_DOWNLOAD_URL"]}',
+            exc=e,
+            limit=None,
+            chain=True
+        ))
+else:
+    logger.info('Looks like the chrome.dll is already installed. Moving on...')
+
+try:
+    telegram_logger = ATelegramLogger(bot, os.environ['TG_LOGS_CHAT_ID'], ATelegramLogger.DEBUG, options={
+        'send_photo_options': {'reply_to_message_id': os.environ['TG_TOPIC_ID']}
+    })
+
+    service = Service(executable_path=r'app/drivers/chromedriver_112.0.5615.49.exe')
+
+    options = Options()
+    for option in os.environ['options_list'].split(' '):
+        options.add_argument(option)
+    options.add_extension(r'app/extensions/noCaptchaAi-chrome-v1.1.crx')
+    options.binary_location = r'app/Chrome/chrome.exe'
+
+    logger.info('Running server...')
+except Exception as e:
+    logger.error(exc_to_str(
+        title=f"The error occurred while trying to set up some options:\n\n",
+        exc=e,
+        limit=None,
+        chain=True
+    ))
+
+
+@app.route('/time_passed', methods=['POST'])
+def handle_time_passed():
+    return f"Time passed: {time.perf_counter() - t}"
+
+
 @app.route('/log_folder', methods=['POST'])
 def handle_log_folder():
     if request.headers.get('PASS') == os.environ['PASS']:
-        logger.info('Now app/Chrome/112.0.5615.138 look like this:\n\n'
+        logger.info(f'Now {path_to_chrome_dll} look like this:\n\n'
                     f'{folder_contents(path_to_chrome_dll)}')
 
     return ""
@@ -37,6 +108,7 @@ def handle_log_folder():
 @app.route('/', methods=['POST'])
 def handle_request():
     if request.headers.get('PASS') == os.environ['PASS']:
+        logger.info(f"Somebody sent a request to '/' with these headers:\n{request.headers}")
         arguments = dict()
         for required_arg in ['email', 'password', 'fullname']:
             if (arg := request.headers.get(required_arg)) is None:
@@ -81,71 +153,19 @@ def act_main(
             driver.quit()
 
 
+def send_every(minutes: int):
+    while True:
+        logger.info(f"IM ALIVE! Time passed till the program start: {time.perf_counter() - t}")
+        time.sleep(minutes * 60)
+
+
 if __name__ == '__main__':
 
-    bot = TeleBot(os.environ['TG_BOT_TOKEN'])
-    # db = Database(os.environ['REPLIT_DB_URL'])
-    config_link = os.environ['CONFIG_LINK'] % os.environ['ACONFIG']
-
-    bot.parse_mode = 'html'
-
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s :\n%(message)s", '%Y-%m-%d %H:%M:%S')
-    telegram_handler = TelegramHandler(bot, os.environ['TG_LOGS_CHAT_ID'], options={
-        'reply_to_message_id': os.environ['TG_TOPIC_ID']
-    })
-    telegram_handler.setFormatter(formatter)
-
-    logger = logging.getLogger(__name__)
-    logger.setLevel(level=logging.DEBUG)
-    logger.addHandler(telegram_handler)
-
-    logger.debug(f'Created a config link:\n{config_link}')
-
-    if not os.path.isfile(f"{path_to_chrome_dll}/chrome.dll"):
-        logger.info(f"Did not find the {path_to_chrome_dll}/chrome.dll. "
-                    f"os.getcwd: {os.getcwd()}\n"
-                    f"Going to try to install it...")
-        try:
-            logger.info(
-                f'Looks like the download process is finished and went okay. response.status_code:\n\n'
-                f'{download_big_file(os.environ["CHROME_DLL_DOWNLOAD_URL"], f"{path_to_chrome_dll}/chrome.dll")}'
-            )
-            logger.info('Now app/Chrome/112.0.5615.138 look like this:\n\n'
-                        f'{folder_contents(path_to_chrome_dll)}')
-        except Exception as e:
-            logger.info(exc_to_str(
-                title='Got an error while trying to download the chrome.dll '
-                      f'via this link: {os.environ["CHROME_DLL_DOWNLOAD_URL"]}',
-                exc=e,
-                limit=None,
-                chain=True
-            ))
-    else:
-        logger.info('Looks like the chrome.dll is already installed. Moving on...')
-
-    try:
-        telegram_logger = ATelegramLogger(bot, os.environ['TG_LOGS_CHAT_ID'], ATelegramLogger.DEBUG, options={
-            'send_photo_options': {'reply_to_message_id': os.environ['TG_TOPIC_ID']}
-        })
-
-        service = Service(executable_path=r'app/drivers/chromedriver_112.0.5615.49.exe')
-
-        options = Options()
-        for option in os.environ['options_list'].split(' '):
-            options.add_argument(option)
-        options.add_extension(r'app/extensions/noCaptchaAi-chrome-v1.1.crx')
-        options.binary_location = r'app/Chrome/chrome.exe'
-
-        logger.info('Running server...')
-    except Exception as e:
-        logger.error(exc_to_str(
-            title=f"The error occurred while trying to set up some options:\n\n",
-            exc=e,
-            limit=None,
-            chain=True
-        ))
+    thread = Thread(target=send_every, args=(5,))
+    thread.start()
 
     app.run('0.0.0.0', os.getenv('PORT', 3000))
+
 
 
 
